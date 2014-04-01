a8dc59cd8561d65516a92cec4ed25c5a0dde96f4
JulissaJ <julissa.jansen@gmail.com>
2014-04-01 11:21:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacklife::Application.config.secret_key_base = '427143cba76d12dd03d3f9b6cdf987f4d576f8f57194146010133790b7adb7cd86e373232468ed108d100e823d0000296c1bcd8a0380e98894955175c8b49250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hacklife::Application.config.session_store :cookie_store, key: '_hacklife_session'
