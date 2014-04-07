906c9701a48c1fac7b55dd04e91ef4acb761c743
Bjorn Linder <bjornishungry@hotmail.com>
2014-04-07 11:10:27 -0400

    Added sign out and error for failed sign in

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -14,10 +14,22 @@ feature 'user signs in' , %Q{
     fill_in "Password", with: user.password
 
     click_on "Sign in"
-    save_and_open_page
     
     expect(page).to have_content("You're In!")
     expect(page).to have_content("Sign Out")    
+    
+  end
+  scenario 'User entering wrong information is given error message and asked to enter correct info' do
+    visit root_path
+    user = FactoryGirl.create(:user)
+    click_link "Sign In"
+    fill_in "Email", with: user.email 
+    fill_in "Password", with: "WRONG"
+
+    click_on "Sign in"
+    expect(page).to_not have_content("You're In!")
+    expect(page).to have_content("Invalid email or password.")    
+    
   end
   
 end
