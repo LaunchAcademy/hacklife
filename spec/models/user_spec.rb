9630aea28a5ead5519ac6dd0960eda4574c22651
Daniel Rubio <drubio1989@gmail.com>
2014-04-17 17:46:35 -0400

    Added unit testing spec for user.admin? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,16 @@ describe User do
   it { should_not have_valid(:username).when(nil, '') }
   it { should allow_value(true, false).for(:admin) }
   it { should_not have_valid(:admin).when(nil,'') }
+
+  describe 'admin function' do
+    it 'is not an admin if the the user field is false' do
+      admin = FactoryGirl.create(:user)
+      expect(admin.admin?).to_not be_true
+    end
+
+    it 'is an admin if the user field is true' do
+      admin = FactoryGirl.create(:user, admin: true)
+      expect(admin.admin?).to be_true
+    end
+  end
 end
